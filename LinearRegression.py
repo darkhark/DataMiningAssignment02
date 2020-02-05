d1171c976e3d2a1cfaa7254ca5fd5e54768c44a7
darkhark <joshua.harkness32@gmail.com>
2020-02-05 15:59:40 -0500

    Up to Question 2

--- LinearRegression.py
+++ LinearRegression.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 from scipy import stats
 import pingouin as pg
 from sklearn.linear_model import LinearRegression
+import seaborn as sbn
+pd.set_option('display.max_colwidth', -1)
 
 print("\nReviewing data before analysis\n")
 
@@ -87,7 +89,9 @@ print("\nQuestion 1.1 Answer\n")
 print(p_value)
 
 print("\nQuestion 1.2\n")
-print(pg.corr(x=homeData['GrLivArea'], y=homeData['SalePrice']))
+# pd.options.display.max_columns = 10
+# print(pg.corr(x=homeData['GrLivArea'], y=homeData['SalePrice']))
+print(b1 - 2 * std_err, b1 + 2 * std_err)
 
 print("\nQuestion 1.3\n")
 model = LinearRegression()
@@ -96,3 +100,26 @@ salesPrediction = model.predict([[2000]])
 print(salesPrediction)
 salesPrediction = model.predict([[2500]])
 print(salesPrediction)
+
+print("\nQuestion 1.4\n")
+sbn.residplot(x, y)
+plt.xlabel('GrLivArea')
+plt.ylabel('Residuals')
+plt.show()
+print('Residual Plot plotted')
+
+print("\nQuestion 1.5\n")
+rSquared = r_value ** 2
+print(rSquared)
+
+print("\nQuestion 1.6\n")
+print(r_value)
+
+print("\n---------Question 2--------------\n")
+homeData['ExterQual'] = homeData.ExterQual.map({'Ex': 4, 'Fa': 2, 'Gd': 4, 'TA': 1})
+print(homeData['ExterQual'])
+
+print("\nQuestion 2.1\n")
+
+
+
